9258ccc2005029ec1a8bbc8c5298f4dced53b507
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-02 15:16:04 +0700

    fix bug with client entity

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db"
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
-Class Client < ActiveRecord::Base 
+class Client < ActiveRecord::Base 
 end
 
 get '/' do
